bab51ec7170d011f055ee0a7aceb9e7744ad538d
Nisarg851 <52291990+Nisarg851@users.noreply.github.com>
2021-02-22 01:39:13 -0800

    Update settings.py

--- Form/settings.py
+++ Form/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^yk=x!+23a4x#61yzvac=ucb5-w#e^_g_6sfj4om2otoncuc(('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -120,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+                                                                                                                                                                                
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
